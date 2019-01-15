a1abc2fa5de4e4c34f91160cfc88c9c0e554fc26
Junyang-Liu <basssozi@outlook.com>
2019-01-15 11:35:45 +0800

    pairs

--- Loop.lua
+++ Loop.lua
@@ -19,6 +19,25 @@ function for_loop_test()
 		print(k,v)
 	end
 
+
+	Table = {[1]=1,[2]=2,[3]=1,[4]=2,[5]=1} --数组
+	-- pairs
+	print("======pairs==============")
+	for k,v in pairs(Table) do
+		print(k,v)
+	end
+
+	Table = {[1]=1,[2]=2,[3]=1,[5]=2,[6]=1,}--数组
+	print("======pairs==============")
+	for k,v in pairs(Table) do
+		print(k,v)
+	end
+	
+	Table = {["a"]=1,["b"]=2,[3]=1,[5]=2,[1]=1,}--数组
+	print("======pairs==============")
+	for k,v in pairs(Table) do
+		print(k,v)
+	end
 	-- body
 end
 for_loop_test()
